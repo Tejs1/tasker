6a47be48cef2699cb88e4aece9224a30f159055f
Tejas Thorat <67797070+Tejs1@users.noreply.github.com>
2024-06-21 21:11:29 +0530

    add new user to database

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,54 @@
+"use server";
+
+import { db } from "@/server/db";
+import { currentUser } from "@clerk/nextjs/server";
+
+import { redirect } from "next/navigation";
+import { type NextRequest } from "next/server";
+
+const createNewUser = async (name: string) => {
+  // const searchParams = request.nextUrl.searchParams
+  // const name = searchParams.get("name")
+  console.log(name);
+
+  const user = await currentUser();
+  // timeoout
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  if (user) {
+    try {
+      const match = await db.user.findUnique({
+        where: {
+          clerkId: user.id,
+        },
+      });
+
+      if (!match) {
+        await db.user.create({
+          data: {
+            clerkId: user.id,
+            email: user.emailAddresses[0]?.emailAddress ?? "nomail@email.com",
+            name: name ? decodeURI(name) : "User2",
+          },
+        });
+        console.log("user created");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    redirect("/dashboard");
+  } else {
+    redirect("/sign-in");
+  }
+};
+
+const NewUser = async ({
+  searchParams,
+}: {
+  searchParams: { name: string };
+}) => {
+  await createNewUser(searchParams.name || "User");
+  return <div>...loading</div>;
+};
+
+export default NewUser;
